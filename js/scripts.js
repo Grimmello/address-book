f3cb37c263da6b6433dd9d3731004a9be7ead326
Clifford Grimmell and Chris Wainner <clifford@>
2017-02-28 08:37:33 -0800

    Update address logic

--- js/scripts.js
+++ js/scripts.js
@@ -63,5 +63,8 @@ $(document).ready(function(){
 
     $("input#new-first-name").val("");
     $("input#new-last-name").val("");
+    $("input.new-street").val("");
+    $("input.new-city").val("");
+    $("input.new-state").val("");
   });
 });
